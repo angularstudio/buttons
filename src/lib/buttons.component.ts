e41279b4ba6b3c3a3b6dd44bed4c0d14621c5bc1
Matthew Davis <matthew@matthewdavis.io>
2020-02-26 15:02:56 -0500

    first commit

---
+++ src/lib/buttons.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-buttons',
+  template: `
+    <p>
+      buttons works!
+    </p>
+  `,
+  styles: []
+})
+export class ButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of buttons
+ */
+
+export * from './lib/buttons.service';
+export * from './lib/buttons.component';
+export * from './lib/buttons.module';
